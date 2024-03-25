f846e692541d543e6639a1dde36cb296cb194e5e
nicolariccimaccarini <115147017+nicolariccimaccarini@users.noreply.github.com>
2024-03-25 17:31:38 +0100

    update comments and file name

--- update_index.py
+++ update_index.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 def generate_index(markdown_text):
-    index = "# Indice\n\n"
+    index = "# Index\n\n"
     headers = re.findall(r'(?m)^#+\s+(.*)', markdown_text)
     
     for header in headers:
@@ -23,16 +23,16 @@ def update_index_in_file(file_path):
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(index + '\n' + markdown_text)
 
-        print("Indice aggiornato correttamente.")
+        print("Index updated successfully.")
 
     except FileNotFoundError:
-        print("Il file specificato non è stato trovato.")
+        print("The specified file was not found.")
     except Exception as e:
-        print(f"Si è verificato un errore durante l'aggiornamento dell'indice: {e}")
+        print(f"An error occurred while updating the index: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 update.py <percorso_file_markdown>")
+        print("Usage: python3 update_index.py <markdown_file_path>")
     else:
         file_path = sys.argv[1]
         update_index_in_file(file_path)
